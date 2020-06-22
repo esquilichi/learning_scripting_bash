f5ee10de292fb7d4b876412293b1c5b4178c2246
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2020-06-22 16:54:01 +0200

    Update portScan.sh

--- portScan.sh
+++ portScan.sh
@@ -3,10 +3,11 @@
 trap ctrl_c INT
 
 function ctrl_c() {
-	echo -e "\n\n [*] Saliendo..."
-	exit 0
+        echo -e "\n\n [*] Saliendo..."
+        exit 0
 }
 
-for port in $(seq 1 65535); do 
-	timeout 1 bash -c "echo '' < /dev/tcp/192.168.1.1/$port" 2</dev/null && echo "Port $port - OPEN" &
+
+for port in $(seq 1 65535); do
+        timeout 1 bash -c "echo '' < /dev/tcp/$1/$port" 2</dev/null && echo -e "\n[*] Port $port - OPEN" &
 done; wait
